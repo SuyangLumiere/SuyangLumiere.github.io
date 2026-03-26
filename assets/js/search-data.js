8b48d5d81ab79f936422f1570f14962bafdabd47
SuyangLumiere <157893565+SuyangLumiere@users.noreply.github.com>
2026-03-26 09:00:40 +0000

    Deploying to gh-pages from @ SuyangLumiere/SuyangLumiere.github.io@faf018381ebee3956e1da952b6ea43ca1e2b7b90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%73%75%79%61%6E%67%4C%75%6D%69%65%72%65@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,14 +532,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=oWQSyxYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/RICO_Lumiere", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/SuyangLumiere", "_blank");
         },
       },{
       id: 'light-theme',
